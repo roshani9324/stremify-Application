f7916f29346235ffa05a448bcf0e5fd65f35be4e
roshani9324 <roshanimaurya.in@gmail.com>
2026-07-04 00:44:17 +0530

    updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5000/api"
+    ? "http://localhost:5001/api"
     : "https://stremify-application.onrender.com/api";
 
 export const axiosInstance = axios.create({
